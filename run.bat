6930579c77ae9a8d3ba1a05fbe7be4b04bf89b29
Yoana Dimitrova <y.dimitrova@ucl.ac.uk>
2007-07-13 12:35:40 +0000

    3D flicker run file changed
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@95 cb8a3d95-5132-0410-b650-8d485996f008

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Xmx700M  -cp neuroConstruct_1.0.3.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
+java  -Dsun.java2d.noddraw=true -Xmx700M  -cp neuroConstruct_1.0.3.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
