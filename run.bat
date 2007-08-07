d99695736759900eb9c41b78516c41e67293628c
Padraig Gleeson <p.gleeson@gmail.com>
2007-08-07 14:49:05 +0000

    More info on flicker and startup
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@148 cb8a3d95-5132-0410-b650-8d485996f008

--- run.bat
+++ run.bat
@@ -4,6 +4,9 @@ REM
 REM   Use an altered version of the line below to run the application with extra memory; type java -X for more..
 REM   Choosing a max java heap size of about half your total physical memory is best, errors have been thrown 
 REM   (when 3D view is shown) when app is run with more than this.
+REM
+REM   Note: if there are problems with excessive flickering when showing 3D, try updating the drivers for your
+REM   graphics card, or adding -Dsun.java2d.noddraw=true to the command below.
 REM 
 
 @echo on
--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 
 #    
-#   Use an altered version of the line below to run the application with extra memory; type java -X for more..
-#   Choosing a max java heap size of about half your total physical memory is best, errors have been thrown 
-#   (when 3D view is shown) when app is run with more than this.
+#   Use an altered version of the line below to run the application with extra memory; type java -X for more...
 # 
 
 java -Xmx700M -cp neuroConstruct_1.0.4.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
