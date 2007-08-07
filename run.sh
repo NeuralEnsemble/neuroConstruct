3c0b052e0f9bb1f1b51b9ab978e05dfc9d936381
Padraig Gleeson <p.gleeson@gmail.com>
2007-08-07 14:30:33 +0000

    new run.sh
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@147 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-java -Xmx700M -cp neuroConstruct_1.0.4.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
+
+#    
+#   Use an altered version of the line below to run the application with extra memory; type java -X for more..
+#   Choosing a max java heap size of about half your total physical memory is best, errors have been thrown 
+#   (when 3D view is shown) when app is run with more than this.
+# 
+
 java -Xmx700M -cp neuroConstruct_1.0.4.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
