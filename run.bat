1f6d4e9b3e96d37029d7f7a6ef141251bb64fa9a
Padraig Gleeson <p.gleeson@gmail.com>
2007-08-07 14:28:30 +0000

    New run.bat
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@146 cb8a3d95-5132-0410-b650-8d485996f008

--- run.bat
+++ run.bat
@@ -1,2 +1,10 @@
-java -Xmx700M  -cp neuroConstruct_1.0.4.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
+@echo off
+
+REM    
+REM   Use an altered version of the line below to run the application with extra memory; type java -X for more..
+REM   Choosing a max java heap size of about half your total physical memory is best, errors have been thrown 
+REM   (when 3D view is shown) when app is run with more than this.
+REM 
+
+@echo on
 java -Xmx700M  -cp neuroConstruct_1.0.4.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
