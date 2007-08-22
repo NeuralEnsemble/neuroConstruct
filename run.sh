78fe383de4131926fdeae23c366f3d91c0cb3f33
Padraig Gleeson <p.gleeson@gmail.com>
2007-08-22 15:59:45 +0000

    Changed to v1.0.4.1
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@184 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #   Use an altered version of the line below to run the application with extra memory; type java -X for more...
 # 
 
-java -Xmx700M -cp neuroConstruct_1.0.6.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
+java -Xmx700M -cp neuroConstruct_1.0.4.1.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
