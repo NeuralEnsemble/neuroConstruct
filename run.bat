eea77248ee5abb4d31667388a899014437613bde
Padraig Gleeson <p.gleeson@gmail.com>
2008-01-02 11:32:09 +0000

    Added -Dsun.java2d.noddraw=true option as default
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@281 cb8a3d95-5132-0410-b650-8d485996f008

--- run.bat
+++ run.bat
@@ -1,13 +1,14 @@
 @echo off
 
 REM    
-REM   Use an altered version of the line below to run the application with extra memory; type java -X for more..
-REM   Choosing a max java heap size of about half your total physical memory is best, errors have been thrown 
-REM   (when 3D view is shown) when app is run with more than this.
+REM   Use an altered value with -Xmx in the line below to run the application with extra 
+REM   memory; type java -X for more details. Choosing a max java heap size of about half 
+REM   your total physical memory is best, errors have been thrown (when 3D view is shown)
+REM   when the application is run with more than this.
 REM
-REM   Note: if there are problems with excessive flickering when showing 3D, try updating the drivers for your
-REM   graphics card, or adding -Dsun.java2d.noddraw=true to the command below.
+REM   Note: the -Dsun.java2d.noddraw=true has been added to solve problems with excessive 
+REM   flickering of the Swing components when showing 3D on some Windows systems
 REM 
 
 @echo on
-java -Xmx700M  -cp neuroConstruct_1.0.7.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
+java -Xmx700M  -Dsun.java2d.noddraw=true -cp neuroConstruct_1.0.7.jar ucl.physiol.neuroconstruct.gui.MainApplication %1 %2 %3 %4 %5
