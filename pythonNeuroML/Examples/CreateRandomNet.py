48d8c8927990ed8946538426df192e842d8c4427
Padraig Gleeson <p.gleeson@gmail.com>
2008-05-08 13:37:09 +0000

    Different figs for rand net
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@420 cb8a3d95-5132-0410-b650-8d485996f008

--- pythonNeuroML/Examples/CreateRandomNet.py
+++ pythonNeuroML/Examples/CreateRandomNet.py
@@ -9,18 +9,21 @@
 #
 #
  
-import sys, os, math, random, xml
+import sys, os, math, random, xml, time
  
 sys.path.append("../NeuroMLUtils")
 from NetworkMLUtils import NetworkMLFile
 
 print("Going to create a NetworkML file...")
 
+start = time.time()
+
+
 nmlFile = NetworkMLFile()
 
 newPop = nmlFile.addPopulation("SampleCellGroup", "SampleCell") # Names chosen for easy import into neuroConstruct...
-popSize = 10
-projSize = 10
+popSize = 10000
+projSize = 10000000
 compNodes = 4 # Number of processors to generate for
 
 newProj = nmlFile.addProjection("NetConn_1", "SampleCellGroup", "SampleCellGroup")
@@ -39,6 +42,10 @@ for i in range(popSize):
 for i in range(projSize):
         newProj.addConnection(random.randint(0, popSize-1), random.randint(0, popSize-1))
     
+
+mid = time.time()
+
+print("                                                 Created Python obj containg net in " +str(mid-start)+" seconds")
   
 #filenameX = "../../../temp/test.nml"
 #nmlFile.writeXML(filenameX)     # Create XML based NetworkML file
@@ -46,4 +53,6 @@ for i in range(projSize):
 filenameH = "../../../temp/test.h5"
 nmlFile.writeHDF5(filenameH)     # Create HDF5 based NetworkML file
 
-print("All done! File with "+str(popSize)+" cells, "+str(projSize)+" conns saved to: "+ filenameH)
+end = time.time()
+
+print("All done! File with "+str(popSize)+" cells, "+str(projSize)+" conns saved to: "+ filenameH +"               in " +str(end-mid)+" seconds")
