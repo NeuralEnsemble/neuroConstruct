a437092ca9ae355dd0aae830cc949d6898645709
Padraig Gleeson <p.gleeson@gmail.com>
2008-06-02 13:30:52 +0000

    run uodated
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@438 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-
 #    
 #   Use an altered version of the line below to run the application with extra memory; type java -X for more...
 # 
 
-java -Xmx700M -cp neuroConstruct_1.1.0.jar ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
+
+export CLASSPATH=neuroConstruct_1.1.0.jar:lib/hdf5/jhdf.jar:lib/hdf5/jhdf4obj.jar:lib/hdf5/jhdf5.jar:lib/hdf5/jhdf5obj.jar:lib/hdf5/jhdfobj.jar
+export JAVA_LIBRARY_PATH=lib/hdf5/linux32
+
+java -Xmx700M  -classpath $CLASSPATH -Djava.library.path=$JAVA_LIBRARY_PATH  ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
