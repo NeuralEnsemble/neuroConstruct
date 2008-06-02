41f32154f623c12b7f730df0282dee245173b406
Padraig Gleeson <p.gleeson@gmail.com>
2008-06-02 14:15:51 +0000

    fixed 64 bit check`
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@441 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 
 export CLASSPATH=neuroConstruct_1.1.0.jar:lib/hdf5/jhdf.jar:lib/hdf5/jhdf4obj.jar:lib/hdf5/jhdf5.jar:lib/hdf5/jhdf5obj.jar:lib/hdf5/jhdfobj.jar
 
-machine=`uname -m | grep 64`
+machine=`uname -a | grep 64`
 
 if [ $? -eq 0 ]; then
-	export JAVA_LIBRARY_PATH=lib/hdf5/linux32
-else
 	export JAVA_LIBRARY_PATH=lib/hdf5/linux
+else
+	export JAVA_LIBRARY_PATH=lib/hdf5/linux32
 fi
 
 java -Xmx700M  -classpath $CLASSPATH -Djava.library.path=$JAVA_LIBRARY_PATH  ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
