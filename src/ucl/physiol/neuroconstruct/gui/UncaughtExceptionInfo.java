01c149a19b2ad10dd3938a4555fff283b5807609
Padraig Gleeson <p.gleeson@gmail.com>
2008-06-13 15:28:35 +0000

    Added uncaught exception handler..
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@465 cb8a3d95-5132-0410-b650-8d485996f008

---
+++ src/ucl/physiol/neuroconstruct/gui/UncaughtExceptionInfo.java
@@ -0,0 +1,54 @@
+/**
+ * neuroConstruct
+ *
+ * Software for developing large scale 3D networks of biologically realistic neurons
+ * Copyright (c) 2008 Padraig Gleeson
+ * UCL Department of Physiology
+ *
+ * Development of this software was made possible with funding from the
+ * Medical Research Council
+ *
+ */
+
+
+package ucl.physiol.neuroconstruct.gui;
+
+import java.lang.Thread.*;
+
+import ucl.physiol.neuroconstruct.utils.*;
+
+
+/**
+ * A class to catch uncaught exceptions, especially OutOfMemeory...
+ * @author Padraig Gleeson
+ *  
+ */
+
+public class UncaughtExceptionInfo implements UncaughtExceptionHandler 
+{
+    private static ClassLogger logger = new ClassLogger("UncaughtExceptionInfo");
+    
+   public void uncaughtException(Thread t, Throwable e) 
+   {
+       String error = "Java error: "+ e.getMessage();
+       
+       if (e instanceof OutOfMemoryError)
+       {
+            String run = GeneralUtils.isWindowsBasedPlatform()?"run.bat":"run.sh";
+           
+            error = error + "\n\nJava has run out of available memory for this task.\n\n" +
+               "Note that not all RAM is available to an instance of the JVM when it starts. The amount available is set by the\n" +
+               "-Xmx flag in the command line call to start a Java application. The neuroConstruct installers set this to 250MB\n" +
+               "of RAM by default. If you'd like to use more, use an altered version of "+run+" in the install directory, and run\n" +
+               "this from a shell/command line console.";
+           
+            GuiUtils.showErrorMessage(logger, error, e, null);
+       }
+       else
+       {
+           logger.logError("Uncaught exception", e, true);
+       }
+      
+    }
+
+}
