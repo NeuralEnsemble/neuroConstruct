933c7a4f83c5e8dc4d6051a6b6cb8ea7548df59c
Padraig Gleeson <p.gleeson@gmail.com>
2008-06-24 10:51:47 +0000

    Updated run.sh to appear like run.bat
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@490 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #    
-#   Use an altered version of the line below to run the application with extra memory; type java -X for more...
+#   Use an altered value with -Xmx in the line below to run the application with extra memory; type java -X for more...
 # 
 
 
-export CLASSPATH=neuroConstruct_1.1.0.jar:lib/hdf5/jhdf.jar:lib/hdf5/jhdf4obj.jar:lib/hdf5/jhdf5.jar:lib/hdf5/jhdf5obj.jar:lib/hdf5/jhdfobj.jar:lib/jython/jython.jar
+# Change this line to your install location
+export NC_HOME=$HOME/neuroConstruct
+export NC_VERSION=1.1.1
+
+
+export H5_JAR_DIR=$NC_HOME/lib/hdf5
+export H5_JARS=$H5_JAR_DIR/jhdf.jar:$H5_JAR_DIR/jhdf4obj.jar:$H5_JAR_DIR/jhdf5.jar:$H5_JAR_DIR/jhdf5obj.jar:$H5_JAR_DIR/jhdfobj.jar
+
+export CLASSPATH=$NC_HOME/neuroConstruct_$NC_VERSION.jar:$H5_JARS:$NC_HOME/lib/jython/jython.jar
 
 machine=`uname -a | grep 64`
 
@@ -12,5 +20,8 @@ if [ $? -eq 0 ]; then
 else
 	export JAVA_LIBRARY_PATH=lib/hdf5/linux32
 fi
+
+
+echo $CLASSPATH
 
 java -Xmx700M  -classpath $CLASSPATH -Djava.library.path=$JAVA_LIBRARY_PATH  ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
