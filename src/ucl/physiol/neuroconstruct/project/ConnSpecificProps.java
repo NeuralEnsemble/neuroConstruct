719ec3902d475abf8d6ee5aa1a1439234723ea1d
Padraig Gleeson <p.gleeson@gmail.com>
2008-06-26 17:34:24 +0000

    Added equals and hashcode
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@504 cb8a3d95-5132-0410-b650-8d485996f008

--- src/ucl/physiol/neuroconstruct/project/ConnSpecificProps.java
+++ src/ucl/physiol/neuroconstruct/project/ConnSpecificProps.java
@@ -12,6 +12,8 @@
 
 package ucl.physiol.neuroconstruct.project;
 
+import ucl.physiol.neuroconstruct.utils.GeneralUtils;
+
 
 
 /**
@@ -43,6 +45,43 @@ public class ConnSpecificProps
         this.weight = props.weight;
         this.internalDelay = props.internalDelay;
     }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final ConnSpecificProps other = (ConnSpecificProps) obj;
+        if (this.synapseType != other.synapseType && (this.synapseType == null || !this.synapseType.equals(other.synapseType)))
+        {
+            return false;
+        }
+        if (this.weight != other.weight)
+        {
+            return false;
+        }
+        if (this.internalDelay != other.internalDelay)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 61 * hash + (this.synapseType != null ? this.synapseType.hashCode() : 0);
+        hash = 61 * hash + Float.floatToIntBits(this.weight);
+        hash = 61 * hash + Float.floatToIntBits(this.internalDelay);
+        return hash;
+    }
     
 
     @Override
@@ -56,6 +95,12 @@ public class ConnSpecificProps
     {
         return "Props for "+synapseType
             +": int del: "+internalDelay+" ms, weight: "+weight+"";
+    }
+
+    public String details(boolean html)
+    {
+        return "Props for "+GeneralUtils.getBold(synapseType, html)
+            +": int del: "+GeneralUtils.getBold(internalDelay, html)+" ms, weight: "+GeneralUtils.getBold(weight, html)+"";
     }
 
 
