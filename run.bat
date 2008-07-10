411faa0e4fd4972ac958319cf49707a0329f372b
Padraig Gleeson <p.gleeson@gmail.com>
2008-07-10 13:43:49 +0000

    More of a warning on memory usage
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@545 cb8a3d95-5132-0410-b650-8d485996f008

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 
 REM    
 REM   Use an altered value with -Xmx in the line below to run the application with extra 
-REM   memory; type java -X for more details. Choosing a max java heap size of about half 
-REM   your total physical memory is best, errors have been thrown (when 3D view is shown)
-REM   when the application is run with more than this.
+REM   memory; type java -X for more details. Choosing a max java heap size slightly less 
+REM   than half your total physical memory is best. 
+REM   *** ASKING FOR MORE MEMORY THAN THIS ON WINDOWS HAS LED TO THE APPLICATION CRASHING ***  
 REM
 REM   Note: the -Dsun.java2d.noddraw=true has been added to solve problems with excessive 
 REM   flickering of the Swing components when showing 3D on some Windows systems
