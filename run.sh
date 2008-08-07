e82c3258cc46c80b9fd3b47669f27f96733d663e
Padraig Gleeson <p.gleeson@gmail.com>
2008-08-07 12:45:57 +0000

    updated for new j3d lib includes
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@612 cb8a3d95-5132-0410-b650-8d485996f008

--- run.sh
+++ run.sh
@@ -1,28 +1,49 @@
 #    
-#   Use an altered value with -Xmx in the commandline below to run the application with extra 
-#   memory; type java -X for more...
+#     neuroConstruct run script for Linux/Mac
 # 
 
 
+########################################################################################
 # Change this line to your install location
 export NC_HOME=$HOME/neuroConstruct
+
+#   Use an altered value below to run the application with extra memory; 
+#   type java -X for more info
+export NC_MAX_MEMORY=450M 
+#
+# The rest of the settings below shouldn't have to change
+########################################################################################
+
+
+
+# Current version of neuroConstruct
 export NC_VERSION=1.1.4
 
 
-export H5_JAR_DIR=$NC_HOME/lib/hdf5
-export H5_JARS=$H5_JAR_DIR/jhdf.jar:$H5_JAR_DIR/jhdf4obj.jar:$H5_JAR_DIR/jhdf5.jar:$H5_JAR_DIR/jhdf5obj.jar:$H5_JAR_DIR/jhdfobj.jar
+# Location of jars and native libraries for HDF5
+H5_DIR=$NC_HOME/lib/hdf5
+H5_JARS=$H5_DIR/jhdf.jar:$H5_DIR/jhdf4obj.jar:$H5_DIR/jhdf5.jar:$H5_DIR/jhdf5obj.jar:$H5_DIR/jhdfobj.jar
 
-export CLASSPATH=$NC_HOME/neuroConstruct_$NC_VERSION.jar:$H5_JARS:$NC_HOME/lib/jython/jython.jar
 
+# Location of jars and native libraries for Java 3D
+J3D_DIR=$NC_HOME/lib/j3d
+J3D_JARS=$J3D_DIR/j3dcore.jar:$J3D_DIR/j3dutils.jar:$J3D_DIR/vecmath.jar
+
+
+export CLASSPATH=$NC_HOME/neuroConstruct_$NC_VERSION.jar:$H5_JARS:$J3D_JARS:$NC_HOME/lib/jython/jython.jar
+
+
+# Determine 32bit or 64bit
 machine=`uname -a | grep 64`
 
 if [ $? -eq 0 ]; then
-	export JAVA_LIBRARY_PATH=lib/hdf5/linux
+	export JAVA_LIBRARY_PATH=$H5_DIR/linux:$J3D_DIR/linux64
 else
-	export JAVA_LIBRARY_PATH=lib/hdf5/linux32
+	export JAVA_LIBRARY_PATH=$H5_DIR/linux32:$J3D_DIR/linux32
 fi
 
 
-echo $CLASSPATH
+echo "Classpath is" $CLASSPATH
+echo "java.library.path is" $JAVA_LIBRARY_PATH
 
-java -Xmx700M  -classpath $CLASSPATH -Djava.library.path=$JAVA_LIBRARY_PATH  ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
+java -Xmx$NC_MAX_MEMORY  -classpath $CLASSPATH -Djava.library.path=$JAVA_LIBRARY_PATH  ucl.physiol.neuroconstruct.gui.MainApplication $1 $2 $3 $4 $5
