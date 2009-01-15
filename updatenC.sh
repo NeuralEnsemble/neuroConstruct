bc78ae61007765ab7fc31730bf4a8c7cc4990e56
Padraig Gleeson <p.gleeson@gmail.com>
2009-01-15 15:08:27 +0000

    Updated update script for linux
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@869 cb8a3d95-5132-0410-b650-8d485996f008

--- updatenC.sh
+++ updatenC.sh
@@ -7,6 +7,8 @@
 export NML_EX_DIR=templates/xmlTemplates/Examples
 export NML_SC_DIR=templates/xmlTemplates/Schemata
 
+export NC_EXAMPLES=nCexamples
+export NC_MODELS=nCmodels
 
 if [ ! -d $NML_EX_DIR ]; then
     svn co https://neuroml.svn.sourceforge.net/svnroot/neuroml/trunk/web/NeuroMLFiles/Examples/ $NML_EX_DIR
@@ -24,5 +26,26 @@ echo "Updating the schema files from the NeuroML Sourceforge repository..."
 svn update $NML_SC_DIR
 
 
+
 echo Updating the main neuroConstruct code...
 svn update
+
+
+
+if [ ! -d $NC_EXAMPLES ]; then
+    svn co svn://87.106.103.176:3999/models/examples/trunk/nCexamples $NC_EXAMPLES
+fi
+
+echo Updating the neuroConstruct core functionality examples
+svn update $NC_EXAMPLES
+
+
+if [ ! -d $NC_MODELS ]; then
+    svn co svn://87.106.103.176:3999/models/examples/trunk/nCmodels $NC_MODELS
+fi
+
+echo Updating the neuroConstruct detailed model examples
+svn update $NC_MODELS
+
+
+
