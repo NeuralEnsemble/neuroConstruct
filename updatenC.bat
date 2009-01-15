058d201c2f77b489b2bc37a46e520a52201c9d82
Padraig Gleeson <p.gleeson@gmail.com>
2009-01-15 14:58:11 +0000

    Updated update script to include nCexamples and nCmodels
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@866 cb8a3d95-5132-0410-b650-8d485996f008

--- updatenC.bat
+++ updatenC.bat
@@ -8,6 +8,10 @@ REM  to date. Note this script may change as the location of the repositories ch
 set NML_EX_DIR=templates\xmlTemplates\Examples
 set NML_SC_DIR=templates\xmlTemplates\Schemata
 
+set NC_EXAMPLES=nCexamples
+set NC_MODELS=nCexamples
+
+
 if not exist %NML_EX_DIR% (
     svn co https://neuroml.svn.sourceforge.net/svnroot/neuroml/trunk/web/NeuroMLFiles/Examples/ %NML_EX_DIR%
 )
@@ -24,5 +28,24 @@ echo Updating the schema files from the NeuroML Sourceforge repository...
 svn update %NML_SC_DIR%
 
 
+
+
 echo Updating the main neuroConstruct code...
 svn update
+
+
+if not exist %NC_EXAMPLES% (
+    svn co svn://87.106.103.176:3999/models/examples/trunk/nCexamples %NC_EXAMPLES%
+)
+
+echo Updating the neuroConstruct core functionality examples
+svn update %NC_EXAMPLES%
+
+
+if not exist %NC_MODELS% (
+    svn co svn://87.106.103.176:3999/models/examples/trunk/nCmodels %NC_MODELS%
+)
+
+
+echo Updating the neuroConstruct detailed model examples
+svn update %NC_MODELS%
