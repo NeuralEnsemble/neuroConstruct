04c90a6d35bc0cc201199e473f5852cd6f416084
Padraig Gleeson <p.gleeson@gmail.com>
2010-08-19 19:22:20 +0000

    Utility for sim information
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@1325 cb8a3d95-5132-0410-b650-8d485996f008

--- pythonNeuroML/nCUtils/simInfo.py
+++ pythonNeuroML/nCUtils/simInfo.py
@@ -6,46 +6,51 @@ simulationInfoFilename = "simulation.props"
 def getValue(line):
 
     if ">" in line:
-        return line.partition(">")[2].partition("<")[0]
+        return line.split(">")[1].split("<")[0].strip()
 
-    return line.partition("=")[2].strip()
+    return line.split("=")[1].strip()
 
 print
 
 if not os.path.isfile(simulationInfoFilename):
-    print "%s doesn't exist!"%simulationInfoFilename
-    exit()
+    print "  File: %s doesn't exist!"%simulationInfoFilename
 else:
 
     simulationInfo = open(simulationInfoFilename, 'r')
 
     for line in simulationInfo:
         if '"dt"' in line:
-            print "dt:                  %s ms"%getValue(line)
+            print "  dt:                  %s ms"%getValue(line)
         if 'opulat' in line:
-            print "Populations:         %s"%getValue(line)
+            pops = getValue(line)
+            popInfo = "  Populations:         "
+            for pop in pops.split(";"):
+                if not pops.startswith(pop):
+                    popInfo = popInfo + "\n                      "
+                popInfo = popInfo +pop
+
+            print popInfo
         if 'Duration' in line:
-            print "Duration:            %s ms"%getValue(line)
+            print "  Duration:            %s ms"%getValue(line)
+        if '"Parallel configuration"' in line:
+            print "  Parallel config:     %s"%getValue(line)
 
     print
     simulatorInfoFilename = "simulator.props"
 
     if not os.path.isfile(simulatorInfoFilename):
-        print "%s doesn't exist. Simulation not yet completed..."%simulatorInfoFilename
-        exit()
+        print "  File: %s doesn't exist. Simulation not yet completed..."%simulatorInfoFilename
     else:
         simulatorInfo = open(simulatorInfoFilename, 'r')
 
         for line in simulatorInfo:
             if 'RealSimulationTime' in line:
                  time = float(getValue(line))/60
-                 print "Sim time:            %g mins"% (time)
+                 print "  Sim time:            %g mins"% (time)
             if 'NumberHosts' in line:
-                print "Number of hosts:     %s"% (getValue(line))
+                print "  Number of hosts:     %s"% (getValue(line))
             if 'Host=' in line:
-                print "Hosts:               %s"% (getValue(line))
-
-
+                print "  Hosts:               %s"% (getValue(line))
 
 print
         
