f41011febc97d4fe2545912e2554f8dbaef92a94
Padraig Gleeson <p.gleeson@gmail.com>
2010-11-26 14:22:50 +0000

    Adding lems path to updatenC.bat
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@1399 cb8a3d95-5132-0410-b650-8d485996f008

--- updatenC.bat
+++ updatenC.bat
@@ -8,6 +8,7 @@ REM  to date. Note this script may change as the location of the repositories ch
 set NML_EX_DIR=templates\xmlTemplates\Examples
 set NML_SC_DIR=templates\xmlTemplates\Schemata
 set SBML2NEU_SC_DIR=templates\SBML2NEURON
+set LEMS_SC_DIR=lems
 
 set NC_EXAMPLES=nCexamples
 set NC_MODELS=nCmodels
@@ -32,6 +33,13 @@ if not exist %SBML2NEU_SC_DIR% (
 )
 echo Updating the SBML2NEURON files from the NeuroML Sourceforge repository...
 svn update %SBML2NEU_SC_DIR%
+
+
+if not exist %LEMS_SC_DIR% (
+    svn co https://neuroml.svn.sourceforge.net/svnroot/neuroml/DemoVer2.0/lems %LEMS_SC_DIR%
+)
+echo Updating the LEMS/NeuroML 2 files from the NeuroML Sourceforge repository...
+svn update %LEMS_SC_DIR%
 
 
 
