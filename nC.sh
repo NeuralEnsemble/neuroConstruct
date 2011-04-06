00465ecf99fdd8dfb952bf9d8bf46e5581005cda
Padraig Gleeson <p.gleeson@gmail.com>
2011-04-06 17:33:46 +0000

    Updating to version 1.5.1
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@1437 cb8a3d95-5132-0410-b650-8d485996f008

--- nC.sh
+++ nC.sh
@@ -21,7 +21,7 @@ export NC_MAX_MEMORY=450M
 
 
 # Current version of neuroConstruct
-export NC_VERSION=1.5.0
+export NC_VERSION=1.5.1
 
 if [ -n "$NC_MAX_MEMORY_LOCAL" ]; then
     export NC_MAX_MEMORY=$NC_MAX_MEMORY_LOCAL
