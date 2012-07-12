223ead8ce057ca3297244af5a4e44ae845ff3d63
Padraig Gleeson <p.gleeson@gmail.com>
2012-07-12 18:22:35 +0000

    Latest script for pulling OSB projects from GitHub
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@1666 cb8a3d95-5132-0410-b650-8d485996f008

--- osb/checkOsb.sh
+++ osb/checkOsb.sh
@@ -20,37 +20,59 @@ standardGHProject()
 {
     echo
     echo "-----  Checking:" $1
+    tput setaf 1
 
-    if [ ! -d $1 ]; then
-        git clone git@github.com:OpenSourceBrain/$1.git
+    if [ ! -d $2 ]; then
+        parent=$2
+        parent=${parent%/*}
+        if [ ! -d $parent ]; then
+            mkdir $parent
+        fi
+        mkdir $2
+        
+    fi
+    tgtDir=$2/$1
+    
+    if [ ! -d $tgtDir ]; then
+        git clone git@github.com:OpenSourceBrain/$1.git $tgtDir
     fi
 
-    pushd $1 > /dev/null
+    pushd $tgtDir > /dev/null
     if $pull; then
         git pull
     else
         git status
+        tput setaf 2
         git fetch --dry-run
     fi
+    tput sgr0
 
     popd > /dev/null
 }
 
 
-standardGHProject 'CA1PyramidalCell'
-standardGHProject 'CerebellarNucleusNeuron'
-standardGHProject 'GranCellLayer'
-standardGHProject 'GranCellRothmanIf'
-standardGHProject 'GranCellSolinasEtAl10'
-standardGHProject 'GranuleCell'
-standardGHProject 'GranuleCellVSCS'
-standardGHProject 'IzhikevichModel'
-standardGHProject 'MainenEtAl_PyramidalCell'
-standardGHProject 'PurkinjeCell'
-standardGHProject 'RothmanEtAl_KoleEtAl_PyrCell'
-standardGHProject 'SolinasEtAl-GolgiCell'
-standardGHProject 'Thalamocortical'
-standardGHProject 'VervaekeEtAl-GolgiCellNetwork'
+standardGHProject 'CA1PyramidalCell' 'hippocampus/CA1_pyramidal_neuron'
+
+standardGHProject 'SolinasEtAl-GolgiCell' 'cerebellum/cerebellar_golgi_cell'
+
+standardGHProject 'CerebellarNucleusNeuron' 'cerebellum/cerebellar_nucleus_cell'
+
+standardGHProject 'GranCellRothmanIf' 'cerebellum/cerebellar_granule_cell'
+standardGHProject 'GranCellSolinasEtAl10' 'cerebellum/cerebellar_granule_cell'
+standardGHProject 'GranuleCell' 'cerebellum/cerebellar_granule_cell'
+standardGHProject 'GranuleCellVSCS' 'cerebellum/cerebellar_granule_cell'
+
+standardGHProject 'PurkinjeCell' 'cerebellum/cerebellar_purkinje_cell'
+
+standardGHProject 'GranCellLayer' 'cerebellum/networks'
+standardGHProject 'VervaekeEtAl-GolgiCellNetwork' 'cerebellum/networks'
+
+standardGHProject 'MainenEtAl_PyramidalCell' 'cerebral_cortex/neocortical_pyramidal_neuron'
+standardGHProject 'RothmanEtAl_KoleEtAl_PyrCell' 'cerebral_cortex/neocortical_pyramidal_neuron'
+
+standardGHProject 'IzhikevichModel' 'cerebral_cortex/networks'
+standardGHProject 'Thalamocortical' 'cerebral_cortex/networks'
+
 
 popd > /dev/null
 
