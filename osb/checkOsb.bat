e35bfb3d9a21fabd0ecebb7ebb0708d15dd706fc
Padraig Gleeson <p.gleeson@gmail.com>
2012-07-30 15:44:39 +0000

    Latest osb for win
    git-svn-id: svn://neuroconstruct.org:3999/neuroConstruct/trunk@1669 cb8a3d95-5132-0410-b650-8d485996f008

--- osb/checkOsb.bat
+++ osb/checkOsb.bat
@@ -6,6 +6,24 @@ REM *** Main
 call :standardGHProject CA1PyramidalCell hippocampus CA1_pyramidal_neuron %1
 call :standardGHProject SolinasEtAl-GolgiCell cerebellum cerebellar_golgi_cell %1
 
+call :standardGHProject CerebellarNucleusNeuron cerebellum cerebellar_nucleus_cell %1
+
+call :standardGHProject GranCellRothmanIf cerebellum cerebellar_granule_cell %1
+call :standardGHProject GranCellSolinasEtAl10 cerebellum cerebellar_granule_cell %1
+call :standardGHProject GranuleCell cerebellum cerebellar_granule_cell %1
+call :standardGHProject GranuleCellVSCS cerebellum cerebellar_granule_cell %1
+
+call :standardGHProject PurkinjeCell cerebellum cerebellar_purkinje_cell %1
+
+call :standardGHProject GranCellLayer cerebellum networks %1
+call :standardGHProject VervaekeEtAl-GolgiCellNetwork cerebellum networks %1
+
+call :standardGHProject MainenEtAl_PyramidalCell cerebral_cortex neocortical_pyramidal_neuron %1
+call :standardGHProject RothmanEtAl_KoleEtAl_PyrCell cerebral_cortex neocortical_pyramidal_neuron %1
+
+call :standardGHProject IzhikevichModel cerebral_cortex networks %1
+call :standardGHProject Thalamocortical cerebral_cortex networks %1
+
 goto :EOF
 
 REM ***********************************
