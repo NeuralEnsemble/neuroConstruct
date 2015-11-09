34fb9535653f467239826af3b42a7d8404f4e430
Padraig Gleeson <p.gleeson@gmail.com>
2015-11-09 10:00:58 +0000

    Add Smith et al L23 cell

--- osb/checkOsb.sh
+++ osb/checkOsb.sh
@@ -208,6 +208,7 @@ standardGHProject 'L5bPyrCellHayEtAl2011' 'cerebral_cortex/neocortical_pyramidal
 standardGHProject 'LarkumEtAl2009' 'cerebral_cortex/neocortical_pyramidal_neuron'
 standardGHProject 'FarinellaEtAl_NMDAspikes' 'cerebral_cortex/neocortical_pyramidal_neuron'
 standardGHProject 'korngreen-pyramidal' 'cerebral_cortex/neocortical_pyramidal_neuron'
+standardGHProject 'SmithEtAl2013-L23DendriticSpikes' 'cerebral_cortex/neocortical_pyramidal_neuron'
 standardGHProject 'dLGNinterneuronHalnesEtAl2011' 'thalamus/lgn_interneuron'
 
 standardGHProject 'IzhikevichModel' 'cerebral_cortex/networks'
